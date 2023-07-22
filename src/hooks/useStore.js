06f8ac36705401e2826ac7a42a40aa495bf14abe
Egor Piskunou <egor.piskunou@gmail.com>
2023-07-22 10:12:21 +0200

    add cubes components

--- src/hooks/useStore.js
+++ src/hooks/useStore.js
@@ -3,11 +3,18 @@ import { nanoid } from 'nanoid'
 
 export const useStore = create((set) => ({
   textures: 'dirt',
-  cubes: [{
-    key: nanoid(),
-    pos: [1, 1, 1],
-    texture: 'dirt',
-  }],
+  cubes: [
+    {
+      key: nanoid(),
+      pos: [2, 0.5, 0],
+      texture: 'dirt',
+    },
+    {
+      key: nanoid(),
+      pos: [1, 0.5, 0],
+      texture: 'wood',
+    }
+  ],
   addCube: (x, y, z) => {
     set((state) => ({
       cubes: [...state.cubes, { key: nanoid(), pos: [x, y, z,], texture: state.texture }],
